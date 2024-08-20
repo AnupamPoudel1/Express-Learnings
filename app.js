379d415b8fc277458733ca2ae5123d58896f2aa5
Anupam <anupampoudel78@gmail.com>
2024-08-20 13:27:34 +0545

    basic json response setup in express.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.use(express.static('./public'));
+const {people } = require('./data');
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'index.html'));
-// })
-
-app.all('*', (req,res) => {
-    console.log(`User Accessed Page That Doesn't Exist`);
-    res.status(404).send('404 Not Found');
-})
+app.get('/', (req, res) => {
+    res.json(people);
+});
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-}) 
+    console.log('Server is running on port 5000.....');
+});
---
+++ data.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: 1,
+        productName: "Nike Air Jordans",
+        productCategory: 'Shoes',
+        price: 25000
+    },
+    {
+        id: 2,
+        productName: "Denim Blue Jeans",
+        productCategory: 'Pants',
+        price: 25000
+    },
+    {
+        id: 3,
+        productName: "Polo Red Shirt",
+        productCategory: 'Shirt',
+        price: 25000
+    },
+]
+
+const people = [
+    {
+        id: 1,
+        name: 'Anupam Poudel',
+        age: 23,
+        sex: 'male',
+    },
+    {
+        id: 2,
+        name: 'Rose Ghimire',
+        age: 23,
+        sex: 'female'
+    }
+]
+
+module.exports = { products, people };
