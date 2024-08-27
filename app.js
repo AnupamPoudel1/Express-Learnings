cd64979c3d08aaf386925a9d35636050ac6b8349
Anupam <anupampoudel78@gmail.com>
2024-08-27 12:03:20 +0545

    simple express middleware setup and understanding middleware

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
 const express = require('express');
-
 const app = express();
 
-const { products } = require('./data');
+// Middleware Learnings
+// req => middleware => res
 
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>Home Page</h1> <a href="/api/products">Products</a>')
-});
+// simple middleware example
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time);
+    next();
+}
 
-app.get('/api/products', (req, res) => {
-
-    const newProducts = products.map((product) => {
-        const { id, productName, price } = product;
-        return { id, productName, price };
-    })
-
-    res.json(newProducts);
+app.get('/', logger, (req, res) => {
+    res.status(200).send(`Home Page`);
 })
 
-app.get('/api/products/:productId', (req, res) => {
-    const { productId } = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productId));
-
-    if (!singleProduct) {
-        res.status(404).send('Product Does Not Exist');
-    }
-
-    res.json(singleProduct);
-})
-
-app.get('/api/v1/products/query', (req, res) => {
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.productName.startsWith(search)
-        });
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if(sortedProducts.length < 1){
-        // res.status(200).send('No Products That Match Your Search');
-        return res.status(200).json({
-            success: true,
-            data: []
-        });
-    }
-    res.status(200).json(sortedProducts);
+app.get('/about', logger, (req, res) => {
+    res.status(200).send(`About Page`);
 })
 
 app.listen(5000, () => {
